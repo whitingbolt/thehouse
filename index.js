0a34addf88180e6c4ed2f8df0bc900a025faa51c
whitingbolt <whitingbolt@gmail.com>
2014-06-19 14:55:44 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
 		<body>
 			<script type="text/javascript">
 				if (confirm("Are you 13 or older?")) {
-					window.location = "index2.html"
+					window.location = "file:///C:/Users/Sammy/Desktop/The_House.html"
 				} else {
 					window.location = "http://www.youtube.com/watch?v=dQw4w9WgXcQ"
 				}
